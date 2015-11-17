47d9589490d7a2217eb00957652d322ae60f2024
Krish Munot <krishmunot@gmail.com>
2015-11-17 23:55:42 +0530

    problem update

---
+++ Diagonal-Difference.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    int n, A[102][102], d1 = 0, d2 = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> A[i][j];
+            if(i==j)
+                d1+=A[i][j];
+            if(i+j==n-1)
+                d2+=A[i][j];
+        }
+    }
+    cout << abs(d1-d2) << endl;
+    return 0;
+}
