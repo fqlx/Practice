8d01d7c207e4546ca3132468f01103d59ad17a16
Krish Munot <krishmunot@gmail.com>
2016-02-14 19:37:46 +0000

    Day 7

---
+++ HackerRank/7 Days of JS/wavy.js
@@ -0,0 +1,15 @@
+function processData(input)
+{
+console.log(Array(15).join(Array(8).join("\u2571\u2572")+'\n'));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
