46d36d6bbaedd2eb3d05937d30e164bec90bba18
Krish Munot <krishmunot@gmail.com>
2016-03-06 14:26:36 +0530

    adding the myfilter fun

--- HackerRank/Javascript---Week-2/4.callbacks.js
+++ HackerRank/Javascript---Week-2/4.callbacks.js
@@ -1,1 +1,7 @@
-processData = (inputArray) =>  console.log(inputArray.filter(x => x%2 == 0)); 
+function myFilter(my_array, callback){
+   return my_array.filter(callback);  
+}
+
+function processData(inputArray) {
+    console.log(myFilter(inputArray, (x) => (x % 2 === 0)));   
+} 
